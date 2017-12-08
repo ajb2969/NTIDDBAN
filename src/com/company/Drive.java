4b818d403ab408c59fa7550aeac7deab7b74207b
pcs7709 <pcs7709@rit.edu>
2017-12-08 11:50:08 -0500

    Added the Drive.java class to make a drive object when we take in all drives at system start.  This will allow us to get information about the drives as we need.

---
+++ src/com/company/Drive.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Drive {
+    private String letterName;
+    private String named;
+    private String model;
+    private String format;
+    private int size;
+
+    public Drive(String name, String namedDrive, String hdModel, String hdFormat, int sizeDrive) {
+        letterName = name;
+        if (namedDrive != "") {
+            named = namedDrive;
+        } else {
+            named = "None";
+        }
+        model = hdModel;
+        format = hdFormat;
+        size = sizeDrive;
+    }
+
+    public String getLetterName() {
+        return letterName;
+    }
+
+    public String getNamed() {
+        return named;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
