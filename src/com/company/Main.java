06df286ea5b4323518fa66ffbb3c78db26b93c0b
pcs7709 <pcs7709@rit.edu>
2017-12-13 13:03:55 -0500

    Able to get drive names as well as their size in bytes?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
+
 import com.company.Drive;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +14,7 @@ import java.util.Observer;
 public class Main extends Application implements Observer {
     private WiperModel model;
     private ArrayList<Drive> drives;
+
     @Override
     public void update(Observable o, Object arg) {
 
@@ -23,19 +26,20 @@ public class Main extends Application implements Observer {
     }
 
     @Override
-    public void init() throws Exception{
+    public void init() throws Exception {
         System.out.println("inti: Initialize and connect to model!");
         model = new WiperModel();
         model.addObserver(this);
         File[] roots = File.listRoots();
         drives = new ArrayList<Drive>();
-        for(int i = 0; i < roots.length; i++){
+        for (int i = 0; i < roots.length; i++) {
             System.out.println("Root[" + i + "]: " + roots[i]);
-            Drive drive = new Drive(roots[i].toString(), "","SSD","exFat", 128);
+            FileSystemView fsv = FileSystemView.getFileSystemView();
+            Drive drive = new Drive(roots[i].toString(), fsv.getSystemDisplayName(roots[i]), "SSD", "exFat", (int) roots[i].getTotalSpace());
             drives.add(drive);
         }
-        for(Drive d : drives){
-            System.out.println(d.getLetterName());
+        for (Drive d : drives) {
+            System.out.println(d.getLetterName() + " " + d.getNamed() + " " + d.getSize());
         }
     }
 
